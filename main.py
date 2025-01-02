3b6cd0c58c94d0b9bf6af5b81a7da974da6fbe36
Michael <michaelsb065@gmail.com>
2025-01-01 23:22:50 -0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from calculadora import Calculadora
 
+
+
 if __name__ == '__main__':
     calculadora = Calculadora()
-    calculadora.entrada()
+#    calculadora.entrada()
     print(calculadora.resultado())
